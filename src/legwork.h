d07d11cdf4fd01387d40c32969aaae78b83526f0
Ben Scott <gamepoet@gmail.com>
2018-04-13 22:18:10 -0700

    typedef the structs
    i thought i could handle it without the typedefs, but i was wrong :(

--- src/legwork.h
+++ src/legwork.h
@@ -4,7 +4,7 @@
 extern "C" {
 #endif
 
-struct legwork_config_t {
+typedef struct legwork_config_t {
   // The number of fibers to create for the pool.
   unsigned int fiber_count;
 
@@ -14,27 +14,27 @@ struct legwork_config_t {
   // The number of worker threads to create that will execute the fibers. Defaults to the number of hardware threads
   // supported by the current hardware.
   unsigned int worker_thread_count;
-};
+} legwork_config_t;
 
-struct legwork_counter_t;
+typedef struct legwork_counter_t legwork_counter_t;
 
 typedef void (*legwork_task_func_t)(void* arg);
 
 // TODO: do we really need to require the caller to pack these together in a struct?
-struct legwork_task_t {
+typedef struct legwork_task_t {
   legwork_task_func_t func;
   void* arg;
-};
+} legwork_task_t;
 
-void legwork_config_init(struct legwork_config_t* config);
-void legwork_init(const struct legwork_config_t* config);
+void legwork_config_init(legwork_config_t* config);
+void legwork_init(const legwork_config_t* config);
 void legwork_shutdown();
 
-void legwork_counter_init(struct legwork_counter_t* counter);
+void legwork_counter_init(legwork_counter_t* counter);
 
-void legwork_task_add(const struct legwork_task_t* tasks, unsigned int task_count, struct legwork_counter_t** counter);
+void legwork_task_add(const legwork_task_t* tasks, unsigned int task_count, legwork_counter_t** counter);
 
-void legwork_wait(struct legwork_counter_t* counter, unsigned int value);
+void legwork_wait(legwork_counter_t* counter, unsigned int value);
 
 #ifdef __cplusplus
 }
